0ad82be865c93fc3e292d00108e24fed667ab951
Tim <timevko@gmail.com>
2014-02-25 22:32:53 -0500

    initial

---
+++ dev/js/main.js
@@ -0,0 +1,9 @@
+//Main Javascripts
+
+//if you have to use jQuery, use the wrapper below. It wraps jQuery in its own object so that it's syntax won't be confused with any other scripts
+
+(function($ , window , undefined) {
+    $(function() {
+        //put jQuery stuff here
+    });
+})(jQuery , window );
---
+++ gulpfile.js
@@ -0,0 +1,62 @@
+// Include gulp and plugins
+var gulp = require('gulp'),
+    autoprefixer = require('gulp-autoprefixer'),
+    minifycss = require('gulp-minify-css'),
+    uglify = require('gulp-uglify'),
+    imagemin = require('gulp-imagemin'),
+    minifycss = require('gulp-minify-css'),
+    clean = require('gulp-clean'),
+    concat = require('gulp-concat'),
+    browserSync = require('browser-sync'),
+    cache = require('gulp-cache');
+// then come the individual functions
+
+//css
+gulp.task('styles', function() {
+  return gulp.src('dev/css/styles.css')
+    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+    .pipe(gulp.dest('assets/css'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(minifycss())
+    .pipe(gulp.dest('assets/css'));
+});
+
+//js
+gulp.task('scripts', function() {
+  return gulp.src('dev/js/*.js')
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest('assets/js'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(uglify())
+    .pipe(gulp.dest('assets/js'));
+});
+
+//image compression
+gulp.task('images', function() {
+  return gulp.src('dev/img/*')
+    .pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
+    .pipe(gulp.dest('assets/img'));
+});
+
+//browser-sync stuff
+gulp.task('browser-sync', function() {
+    browserSync.init(['assets/css/*.css', '*.php','assets/js/*.js']);
+});
+
+//cleanup time
+gulp.task('clean', function() {
+  return gulp.src(['assets/css', 'assets/js', 'assets/img'], {read: false})
+    .pipe(clean());
+});
+
+gulp.task('default', ['clean'], function() {
+    // Run all tasks on first run
+    gulp.run('styles', 'scripts', 'images','borwser-sync');
+    // Watch the css folder for changes
+    gulp.watch('dev/css/*.css', ['styles']);
+    // Watch the js folder for changes
+    gulp.watch('dev/js/*.js', ['scripts']);
+    // Watch the img folder for changes
+    gulp.watch('dev/img/*', ['images']);
+});
+
